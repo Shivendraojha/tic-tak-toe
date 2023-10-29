fab3636de081ac4de81c6fed57fd5dd8b6c63492
Shivendra Ojha <96243341+Shivendraojha@users.noreply.github.com>
2023-10-29 18:38:57 +0000

    comleted tic tak toe

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const initialBoard = Array(9).fill(null);
+
+const App = () => {
+  const [board, setBoard] = useState(initialBoard);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const handleClick = (i) => {
+    const newBoard = [...board];
+    if (calculateWinner(newBoard) || newBoard[i]) {
+      return;
+    }
+    newBoard[i] = xIsNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (i) => {
+    return (
+      <button className="square" onClick={() => handleClick(i)}>
+        {board[i]}
+      </button>
+    );
+  };
+
+  const winner = calculateWinner(board);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  return (
+    <div className="App">
+      <h1>Tic-Tac-Toe</h1>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+export default App;
